79153676a0efd275cd39ca220e7943a489a0d3a5
pallavkalita <165314390+pallavkalita@users.noreply.github.com>
2024-09-30 12:35:37 +0530

    Delete expense_management.py

--- expense_management.py
+++
@@ -1,8 +0,0 @@
-def add_expense(month, categ , descp, amount):
-    list_expense = []
-    month_expense = {}
-    list_expense.append((categ, descp, amount))
-    
-    month_expense[month] = list_expense
-    
-    
