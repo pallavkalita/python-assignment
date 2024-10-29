59dd193c681d8c9a5853c59bbe12c792ff7f199f
pallavkalita <165314390+pallavkalita@users.noreply.github.com>
2024-10-29 13:49:35 +0530

    Add files via upload

---
+++ Python Packages/Task 1 ( Mathematical Operations )/main.py
@@ -0,0 +1,10 @@
+from Operations.arithmetic import add, subtract, multiply, divide
+from Operations.geometric import  area_of_circle, area_of_rectangle
+
+print("Addition:", add(7, 7))
+print("Subtraction:", subtract(20, 8))
+print("Multiplication:", multiply(3, 6))
+print("Division:", divide(20, 2))
+
+print("Area of Circle (radius 3):", area_of_circle(3))
+print("Area of Rectangle (length 4, width 3):", area_of_rectangle(5, 4))
